752715809baf69340812c38cfde30ee1ffdd1bdd
Igor <Ilin>
2025-04-22 08:13:26 +0300

    Added images
    images + corrections

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <main :class="$style.mainContainer">
+    <h1>Wedding day</h1>
+    <!-- <NuxtImg src="/We" /> -->
+
+  </main>
+</template>
+
+<script setup></script>
+
+<style module lang="scss">
+.mainContainer {
+  background-color: #6B9771;
+}
+</style>
